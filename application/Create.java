f570c86376873ba10adfdd4ad77b56d8b0bd4323
Jacinta Zhang <jzha700@en-344792.uoa.auckland.ac.nz>
2019-09-30 10:02:41 +1300

    Some messsage

--- application/Create.java
+++ application/Create.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
@@ -350,16 +351,36 @@ public class Create {
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-				String cMD = "ffmpeg -framerate $((" + _numPics + "))/$(soxi -D temp.wav) -i " + _term + "%02d.jpg -vf \"scale=w=1280:h=720:force_original_aspect_ratio=1,pad=1280:720:(ow-iw)/2:(oh-ih)/2\" -r 25 visual.mp4 ; rm " + _term + "??.jpg ; ffmpeg -i visual.mp4 -vf \"drawtext=fontsize=50:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:borderw=5:text=\'" + _term + "\'\" out.mp4 ; ffmpeg -i out.mp4 -i temp.wav -c:v copy -c:a aac -strict experimental -y " + _name + ".mp4 &>/dev/null ; rm visual.mp4 ; rm out.mp4";
-				ProcessBuilder builderr = new ProcessBuilder("/bin/bash", "-c", cMD);
-				try {
-					Process vidProcess = builderr.start();
-					vidProcess.waitFor();
-				} catch (IOException e) {
-					e.printStackTrace();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
+//				if(_numPics == 1) {
+//					String cMD = "ffmpeg -loop 1 -i " + _term + "%02d.jpg -t $(soxi -D temp.wav) -vf \"scale=w=1280:h=720:force_original_aspect_ratio=1,pad=1280:720:(ow-iw)/2:(oh-ih)/2\" -r 25 visual.mp4 ; rm " + _term + "??.jpg ; ffmpeg -i visual.mp4 -vf \"drawtext=fontsize=50:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:borderw=5:text=\'" + _term + "\'\" out.mp4 ; ffmpeg -i out.mp4 -i temp.wav -c:v copy -c:a aac -strict experimental -y " + _name + ".mp4 &>/dev/null ; rm visual.mp4 ; rm out.mp4";
+//					ProcessBuilder builderr = new ProcessBuilder("/bin/bash", "-c", cMD);
+//					try {
+//						Process vidProcess = builderr.start();
+//						vidProcess.waitFor();
+//					} catch (IOException e) {
+//						e.printStackTrace();
+//					} catch (InterruptedException e) {
+//						e.printStackTrace();
+//					}
+//				}else {
+					String cMD = "cat *.jpg | ffmpeg -f image2pipe -framerate $((" + _numPics + "))/$(soxi -D temp.wav) -i - -c:v libx264 -pix_fmt yuv420p -vf \"scale=w=1280:h=720\" -r 25 -y visual.mp4 ; rm " + _term + "??.jpg ; ffmpeg -i visual.mp4 -vf \"drawtext=fontsize=50:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:borderw=5:text=\'" + _term + "\'\" out.mp4 ; ffmpeg -i out.mp4 -i temp.wav -c:v copy -c:a aac -strict experimental -y " + _name + ".mp4 &>/dev/null ; rm visual.mp4 ; rm out.mp4";
+					ProcessBuilder builderr = new ProcessBuilder("/bin/bash", "-c", cMD);
+					try {
+						Process vidProcess = builderr.start();
+						vidProcess.waitFor();
+						InputStream error = vidProcess.getErrorStream();
+						InputStreamReader isrerror = new InputStreamReader(error);
+						BufferedReader bre = new BufferedReader(isrerror);
+						String line = null;
+						while ((line = bre.readLine()) != null) {
+						        System.out.println(line);
+						    }
+					} catch (IOException e) {
+						e.printStackTrace();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+//				}
 				
 				Platform.runLater(new Runnable(){
 					@Override public void run() {
