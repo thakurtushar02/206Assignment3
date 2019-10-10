c45235086698145d30d234f9c994356f34bb7df1
Tushar <thakur.tushar0205@gmail.com>
2019-10-10 16:24:33 +1300

    UI fixes for kids

--- application/Home.java
+++ application/Home.java
@@ -18,7 +18,7 @@ import javafx.scene.layout.VBox;
 public class Home {
 	private Tab _tab;
 	private int _titleNumber = 0;
-	private final int TITLE_SIZE = 800;
+	private final int TITLE_SIZE = 1000;
 	
 	public Home(Tab tab) {
 		_tab = tab;
@@ -35,6 +35,7 @@ public class Home {
 		titleView.setFitWidth(TITLE_SIZE);
 		
 		HBox titleBox = new HBox(titleView);
+		titleBox.setMinHeight(titleView.getFitHeight() + 100);
 		titleBox.setOnMouseEntered(arg0 -> {
 			titleView.setFitWidth(TITLE_SIZE + 100);
 		});
@@ -50,7 +51,7 @@ public class Home {
 		ImageView infoView = new ImageView();
 		infoView.setImage(new Image(".resources/home/heading.png"));
 		infoView.setPreserveRatio(true);
-		infoView.setFitWidth(1000);
+		infoView.setFitWidth(700);
 		
 		ImageView createHeading = new ImageView();
 		createHeading.setImage(new Image(".resources/home/heading2.png"));
@@ -67,7 +68,13 @@ public class Home {
 		arrow2.setPreserveRatio(true);
 		arrow2.setFitWidth(50);
 		
+		ImageView arrow3 = new ImageView();
+		arrow3.setImage(new Image(".resources/home/arrow.png"));
+		arrow3.setPreserveRatio(true);
+		arrow3.setFitWidth(50);
+		
 		HBox createBox = new HBox(20, arrow, createHeading);
+		createBox.setMinHeight(createHeading.getFitHeight() + 100);
 		createBox.setOnMouseEntered(arg0 -> {
 			createHeading.setFitWidth(600);
 			arrow.setFitWidth(60);
@@ -79,7 +86,6 @@ public class Home {
 		createBox.setOnMouseClicked(arg0 -> {
 			tabPane.getSelectionModel().select(2);
 		});
-		//createBox.setAlignment(Pos.CENTER);
 		
 		ImageView viewHeading = new ImageView();
 		viewHeading.setImage(new Image(".resources/home/heading3.png"));
@@ -87,6 +93,7 @@ public class Home {
 		viewHeading.setFitWidth(500);
 		
 		HBox viewBox = new HBox(20, arrow2, viewHeading);
+		viewBox.setMinHeight(viewHeading.getFitHeight() + 100);
 		viewBox.setOnMouseEntered(arg0 -> {
 			viewHeading.setFitWidth(550);
 			arrow2.setFitWidth(60);
@@ -98,9 +105,27 @@ public class Home {
 		viewBox.setOnMouseClicked(arg0 -> {
 			tabPane.getSelectionModel().select(1);
 		});
-		//viewBox.setAlignment(Pos.CENTER);
+		
+		ImageView playHeading = new ImageView();
+		playHeading.setImage(new Image(".resources/home/heading4.png"));
+		playHeading.setPreserveRatio(true);
+		playHeading.setFitWidth(300);
+		
+		HBox playBox = new HBox(20, arrow3, playHeading);
+		playBox.setMinHeight(playHeading.getFitHeight() + 100);
+		playBox.setOnMouseEntered(arg0 -> {
+			playHeading.setFitWidth(350);
+			arrow3.setFitWidth(60);
+		});
+		playBox.setOnMouseExited(arg0 -> {
+			playHeading.setFitWidth(300);
+			arrow3.setFitWidth(50);
+		});
+		playBox.setOnMouseClicked(arg0 -> {
+			//tabPane.getSelectionModel().select(1);
+		});
 	
-		VBox contents = new VBox(titleBox, infoView, createBox, viewBox);
+		VBox contents = new VBox(titleBox, infoView, createBox, viewBox, playBox);
 		contents.setPadding(new Insets(30,30,30,30));
 		contents.setSpacing(30);
 		contents.setAlignment(Pos.CENTER);
