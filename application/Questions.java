3aa1184580e0962538e60829a99b1547cf0fbb68
Lynette Nguyen <lugn621@aucklanduni.ac.nz>
2019-10-13 18:54:17 +1300

    Added questions created based on past creations

--- application/Questions.java
+++ application/Questions.java
@@ -1,5 +1,9 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -7,6 +11,37 @@ import java.util.Random;
 public class Questions {
 
 	private List<Question> questions = new ArrayList<Question>();
+	
+	public Questions() {
+		
+		List<String> qVideos = new ArrayList<String>();
+		String command = "ls Quizzes/ | grep mp4$";
+		
+		ProcessBuilder pb = new ProcessBuilder("bash", "-c", command);
+		try {
+			Process question = pb.start();
+			BufferedReader stdout = new BufferedReader(new InputStreamReader(question.getInputStream()));
+			
+			int exitStatus = question.waitFor();
+			if (exitStatus == 0) {
+				String line;
+				while ((line = stdout.readLine()) != null) {
+					qVideos.add(line);
+				}
+			}
+			
+			if (! qVideos.isEmpty()) {
+				for (String q: qVideos) {
+					Question term = new Question(new File("Quizzes/"+q), q.substring(0, q.length()-4));
+					this.addQuestion(term);
+				}
+			}
+		} catch (IOException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 	
 	public void addQuestion(Question question) {
 		questions.add(question);
