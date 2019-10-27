f88b7ec84a8e1dad5a24eef0f324a21227136966
Lynette Nguyen <lugn621@aucklanduni.ac.nz>
2019-10-27 15:06:43 +1300

    added comments

--- application/learn/Learn.java
+++ application/learn/Learn.java
@@ -1,6 +1,7 @@
 package application.learn;
 
 import java.io.File;
+
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -18,14 +19,7 @@ import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.BorderWidths;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
@@ -33,9 +27,13 @@ import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-
+/**
+ * This class represents the Learn tab in the VARpedia application. Here the user completes a quiz based
+ * on the creations they have made using the app. 
+ * @author Lynette
+ *
+ */
 public class Learn {
 
 	private Tab tab;
@@ -64,10 +62,16 @@ public class Learn {
 		qSet = set;
 	}
 
+	/**
+	 * Sets the contents of the tab when it is first selected (i.e. the quiz start screen)
+	 */
 	public void setContents() {
-		content.getChildren().removeAll(content.getChildren());
+		content.getChildren().removeAll(content.getChildren()); // remove any nodes that may have been set previously
+		
+		// Quiz unable to start if there are no questions to display
 		if(qSet.numberOfQuestions() == 0) {
-			learn.setText("You don't have any quizzes!");
+			learn.setText("You don't have any quizzes! Head over to the Create tab"
+					+ "and make some creations first");
 			content.setTop(learn);
 		}else {
 			learn.setText("Time to review what you have learned!");
@@ -86,56 +90,17 @@ public class Learn {
 		content.setPadding(new Insets(20));
 		BorderPane.setAlignment(learn, Pos.CENTER);
 		tab.setContent(content);
-
 	}
 
+	/**
+	 * Starts the quiz 
+	 */
 	public void quizStart() {
-		//		qNums.clear();
-		//		answer.clear();
-		//		
-		//		content.getChildren().removeAll(content.getChildren());
-		//		question = qSet.getQuestion();
-		//		
-		//		HBox options = new HBox();
-		//		VBox qNumbers = new VBox();
-		
-		
-
 		correct = 0;
 		current = 1;
 		quizSetContent();
 		
-		//		Label quiz = new Label("What is this?");
-		//		quiz.setFont(new Font("Arial", 16));
-		//		
-		//		for (int i = 1; i < 6; i ++) {
-		//			Button btn = new Button();
-		//			btn.setText(""+i);
-		//			btn.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
-		//			qNums.add(btn);
-		//			qNumbers.getChildren().add(btn);
-		//		}
-		//		
-		//		qNumbers.setAlignment(Pos.CENTER_LEFT);
-		//		qNumbers.setSpacing(10);
-		//		
-		//		
-		//		for (int i = 0; i < 4; i++) {
-		//			ToggleButton btn = new ToggleButton();
-		//			btn.setToggleGroup(ops);
-		//			answer.add(btn);
-		//			options.getChildren().add(btn);
-		//		}
-
-		//		setQuestion(question);
-		//		
-		//		mView.setFitHeight(400);
-		//		mView.setPreserveRatio(true);
-		//		
-		//		Button submit = new Button("Check Answer");
-		//		submit.setVisible(false);
-
-		// Button only appears if an answer is selected
+		// Check button only appears if an answer is selected
 		ops.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
 			@Override
 			public void changed(ObservableValue<? extends Toggle> arg0, Toggle arg1, Toggle arg2) {
@@ -147,19 +112,21 @@ public class Learn {
 			}
 		});
 
-
 		submit.setOnAction(e -> {
 			if (submit.getText() == "Check Answer") {
+				
 				Button btn = qNums.get(current - 1);
 				ToggleButton selection = (ToggleButton) ops.getSelectedToggle();
 				ToggleButton correctAnswer = null;
+				
 				for (ToggleButton t : answer) {
 					if (t.getText().equals(question.getCorrectAnswer())) {
 						correctAnswer = t;
 						break;
 					}
 				}
-
+				
+				// Set the colour of the question number and answer buttons whether it is correct or not
 				if (isCorrect(question)) {
 					btn.setStyle("-fx-background-color:green");
 					selection.setStyle("-fx-border-color:green; ");
@@ -177,6 +144,8 @@ public class Learn {
 				}
 			} else if (submit.getText() == "Next") {
 				nextQuestion();
+				
+				// Reset the styling of the answer buttons after it has been checked
 				for (ToggleButton toggle : answer) {
 					toggle.setStyle(null);
 				}
@@ -187,19 +156,22 @@ public class Learn {
 			}
 
 		});
-
-		
-
 	}
-
+ 
+	/**
+	 * Retrieves and sets the video and text for answer buttons for each question
+	 * @param question Question to be asked
+	 */
 	public void setQuestion(Question question) {
 
+		// Shuffles and sets the text for the answer buttons
 		Collections.shuffle(answer);
 		for (int i = 0; i < 4; i++) {
 			answer.get(i).setText(question.getAnswers().get(i));
 			answer.get(i).setSelected(false);
 		}
 
+		// Retrieve and set the quiz video in the mediaplayer
 		media = new Media(question.getVideo());
 		player = new MediaPlayer(media);
 		player.setAutoPlay(true);
@@ -210,9 +182,14 @@ public class Learn {
 		} else {
 			mView.setMediaPlayer(player);
 		}
-
 	}
 
+	/**
+	 * Checks if the selected answer is correct
+	 * @param	question Question that is being asked
+	 * @return	true if correct, otherwise false
+	 * 		
+	 */
 	public boolean isCorrect(Question question) {
 		ToggleButton selected = (ToggleButton) ops.getSelectedToggle();
 		if (selected.getText() == question.getCorrectAnswer()) {
@@ -221,11 +198,17 @@ public class Learn {
 		return false;
 	}
 
+	/**
+	 * Gets the next question and sets it in the GUI
+	 */
 	public void nextQuestion() {
 		question = qSet.getQuestion();
 		setQuestion(question);
 	}
 
+	/**
+	 * Sets the end screen once the user has completed all the questions 
+	 */
 	public void endScreen() {
 		content.getChildren().removeAll(content.getChildren());
 		end.setText("You got "+correct+"/5!");
@@ -241,7 +224,7 @@ public class Learn {
 		image.setPreserveRatio(true);
 		image.setFitWidth(400);
 
-		playAgain.setText("Play Again!");
+		playAgain.setText("Play Again");
 		playAgain.setOnAction(e -> quizStart());
 
 		content.setTop(end);
@@ -251,13 +234,16 @@ public class Learn {
 		BorderPane.setAlignment(playAgain, Pos.CENTER);
 
 	}
-
+	
+	/**
+	 * Sets the GUI components of the quiz 
+	 */
 	public void quizSetContent() {
+		// clear collections every time quiz starts (in case of replays)
 		qNums.clear();
 		answer.clear();
 
 		content.getChildren().removeAll(content.getChildren());
-		question = qSet.getQuestion();
 
 		HBox options = new HBox();
 		HBox check = new HBox();
@@ -271,7 +257,8 @@ public class Learn {
 		
 		submit.setText("Check Answer");
 		submit.setVisible(false);
-
+		
+		// Generate question numbers to display
 		for (int i = 1; i < 6; i ++) {
 			Button btn = new Button();
 			btn.setText(""+i);
@@ -280,14 +267,23 @@ public class Learn {
 			qNumbers.getChildren().add(btn);
 		}
 
+		// Generate answer toggle buttons
 		for (int i = 0; i < 4; i++) {
 			ToggleButton btn = new ToggleButton();
 			btn.setToggleGroup(ops);
 			answer.add(btn);
 			options.getChildren().add(btn);
 		}
-		setQuestion(question);
+		
+		// Add components to the view
+		content.setTop(quiz);
+		content.setRight(qNumbers);
+		content.setCenter(main);
+		content.setBottom(check);
+		
+		nextQuestion();
 
+		// GUI component configurations
 		mView.setFitHeight(400);
 		mView.setPreserveRatio(true);
 
@@ -306,12 +302,7 @@ public class Learn {
 		qNumbers.setAlignment(Pos.CENTER_LEFT);
 		qNumbers.setSpacing(10);
 
-		content.setTop(quiz);
-		content.setRight(qNumbers);
-		content.setCenter(main);
-		content.setBottom(check);
 		BorderPane.setAlignment(quiz, Pos.CENTER);
-
 		BorderPane.setMargin(quiz, new Insets(10, 10, 10, 0));
 		BorderPane.setMargin(main, new Insets(0, 20, 10, 0));
 		BorderPane.setMargin(qNumbers, new Insets(0,10,10,0));
